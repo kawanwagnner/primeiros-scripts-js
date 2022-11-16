d4980fa63e058cd3ae012ee53263d20cdab25175
Kawan Wagnner <103159338+kawanwagnner@users.noreply.github.com>
2022-11-15 22:28:37 -0300

    estudando condições em js.

---
+++ ex008.js
@@ -0,0 +1,7 @@
+var vel = 60
+console.log('Atenção! Você passou por um radar.')
+if (vel > 60) {
+    console.log('Obs: Ultrapassou a velocidade permitida! MULTADO!')
+} else {
+    console.log('Obs: Você estava na velocidade permitida!')
+} 
---
+++ ex014.js
@@ -0,0 +1,11 @@
+var hora = 23
+console.log(`Agora são ${hora} horas.`)
+if (hora < 5) {
+    console.log ('Boa Madrugada!')
+} else if (hora < 12) {
+    console.log('Bom Dia!')
+} else if (hora < 17) {
+    console.log ('Boa Tarde!')
+} else if (hora < 23) {
+    console.log ('Boa noite!')
+}
