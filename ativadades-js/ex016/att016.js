205ec40aeb2fc340eb5147e2ba1972462739625f
Kawan Wagnner <103159338+kawanwagnner@users.noreply.github.com>
2022-11-22 22:20:05 -0300

    ..

--- ativadades-js/ex016/att016.js
+++ ativadades-js/ex016/att016.js
@@ -1,14 +1,50 @@
 var btn = document.querySelector('#verificar')
 btn.addEventListener('click', () => {
-    var masc = document.querySelector('#masc')
-    var fem = document.querySelector('femi')
-    var nasc = Number(document.querySelector('#nasc'))
+    var data = new Date()
+    var ano = data.getFullYear()
+    var nasc = document.querySelector('#nasc')
     var res = document.querySelector('#res')
-
-    if (nasc == masc <= 2004) 
-    res.innerHTML = "Você é um Jovem"
-    res.src = ''
-
-})
+    if (nasc.value.length == 0 || Number(nasc.value) > ano) {
+        window.alert('[ERRO!] Preencha os dados corretamente.')
+    } else {
+        var sex = document.getElementsByTagName('radiosex').checked = true;
+        var idade = ano - Number(nasc.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (sex[0]) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 12) {
+                //Criança
+                img.getAttribute('src', 'img-att016/masc-crianca.jpg')
+            } else if (idade < 21) {
+                //Jovem
+                img.getAttribute('src', 'img-att016/jovem-m.jpg')
+            } else if (idade < 50) {
+                //Adulto
+                img.getAttribute('src', 'img-att016/jovem-m.jpg')
+            } else {
+                //idoso
+                img.getAttribute('src', 'img-att016/idoso.jpg')
+            }
+        } else if (sex[1]) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 12) {
+                //Criança
+                img.getAttribute('src', 'img-att016/fem-crianca.jpg')
+            } else if (idade < 21) {
+                //Jovem
+                img.getAttribute('src', 'img-att016/jovem-f.jpg')
+            } else if (idade < 50) {
+                //Adulta
+                img.getAttribute('src', 'img-att016/jovem-f.jpg')
+            } else {
+                //idosa
+                img.getAttribute('src', 'img-att016/idosa.jpg')
+            }
+        }
+        res.innerHTML = `Detectamos um ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    }})
 
 
